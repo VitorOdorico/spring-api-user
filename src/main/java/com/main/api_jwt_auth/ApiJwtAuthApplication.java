2756c13bc8c54b7e645f3dcda462d73ae4c0e260
Vitor Emanuel Odorico Neto Costa <VCosta@solucaonetwork.com>
2024-12-23 14:57:12 -0300

    start project

---
+++ src/main/java/com/main/api_jwt_auth/ApiJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package com.main.api_jwt_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJwtAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJwtAuthApplication.class, args);
+	}
+
+}
